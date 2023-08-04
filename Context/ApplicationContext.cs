cab1bd3f2a0f19c6e2a23368ca088f8339e2d12f
duyhelloworld <codedaovoiduy@gmail.com>
2023-08-04 22:03:07 +0700

    context về 1 nhà thôi

---
+++ Context/ApplicationContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using qlsinhvien.Entities;
+
+namespace qlsinhvien.Context;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> dbContextOptions) : base(dbContextOptions)
+    {
+
+    }
+    public DbSet<SinhVien> SinhViens { get; set; }
+    public DbSet<DiemSinhVien> DiemSinhViens { get; set; }
+    public DbSet<GiangVien> GiangViens { get; set; }
+    public DbSet<Khoa> Khoas { get; set; }
+    public DbSet<LopMonHoc> LopMonHocs { get; set; }
+    public DbSet<LopQuanLi> LopQuanLis { get; set; }
+    public DbSet<KhoaMonHoc> KhoaMonHocs { get; set; }
+    public DbSet<MonHoc> MonHocs { get; set; }
+}
