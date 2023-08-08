d53781402b53e698bc214ddbfaa209cfd67262b2
duyhelloworld <codedaovoiduy@gmail.com>
2023-08-08 10:22:52 +0700

    xử lí trả api khó phết

---
+++ Dto/GiangVienContact.cs
@@ -0,0 +1,9 @@
+namespace qlsinhvien.Dto;
+
+public struct GiangVienContact
+{
+    public int MaGiangVien { get; set; }
+    public string TenGiangVien { get; set; }
+    public string Email { get; set; }
+    public string SoDienThoai { get; set; }
+}
