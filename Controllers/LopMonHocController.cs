133aaf5e78670e7a69c8c6fbcf59b2d0cb2c2cd8
Kaizu69 <hiep8ams@gmail.com>
2023-08-09 09:44:18 +0700

    k có j

--- Controllers/LopMonHocController.cs
+++ Controllers/LopMonHocController.cs
@@ -5,13 +5,13 @@ using qlsinhvien.Entities;
 namespace qlsinhvien.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("/[controller]")]
     public class LopMonHocController : ControllerBase
     {
-        private readonly ApplicationContext lopMonHocDbContext;
-        public LopMonHocController(ApplicationContext lopMonHocDbContext)
+        private readonly ApplicationContext appContext;
+        public LopMonHocController(ApplicationContext appContext)
         {
-            this.lopMonHocDbContext = lopMonHocDbContext;
+            this.appContext = appContext;
         }
     }
 }
