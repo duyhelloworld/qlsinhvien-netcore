11f40caaa5258359c1944d30aa089cddad4446a5
duyhelloworld <codedaovoiduy@gmail.com>
2023-08-13 22:03:20 +0700

    vẫn méo biết fix khi add lớp quản lí

--- Controllers/LopQuanLiController.cs
+++ Controllers/LopQuanLiController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using qlsinhvien.Context;
@@ -18,7 +19,8 @@ namespace qlsinhvien.Controllers
 
         [HttpGet("all")]
         public ActionResult GetAll() {
-            return Ok(lopQuanLiDbContext.LopQuanLis.ToList());
+            return Ok(lopQuanLiDbContext.LopQuanLis
+                        .ToList());
         }
 
         [HttpGet]
@@ -27,39 +29,19 @@ namespace qlsinhvien.Controllers
             var ketQua = from lql in lopQuanLiDbContext.LopQuanLis
                 where lql.TenLopQuanLi.Contains(TenLopQuanLi)
                 select lql;
-            return ketQua.Count() == 0 ? NotFound() : Ok(ketQua);
-        }
-
-        [HttpGet("{MaLopQuanLi}")]
-        public ActionResult<LopQuanLi> GetById(int MaLopQuanLi)
-        {
-            var ketQua = lopQuanLiDbContext.LopQuanLis
-                .Find(MaLopQuanLi);
             return ketQua == null ? NotFound() : Ok(ketQua);
         }
 
-        [HttpPost]
-        public ActionResult AddLopQL([FromBody] LopQuanLi LopQuanLi)
+        [HttpGet("{malop}")]
+        public ActionResult<LopQuanLi> GetById(int malop)
         {
-            if (LopQuanLi.MaLopQuanLi != 0)
-            {
-                return BadRequest();
-            }
-            // try
-            // {
-                lopQuanLiDbContext.LopQuanLis.Add(LopQuanLi);
-                lopQuanLiDbContext.SaveChanges();
-                return Created(nameof(GetById), LopQuanLi);
-            // }
-            // catch (DbUpdateException e)
-            // {
-            //     Console.WriteLine(e.Message);
-            //     return BadRequest();
-            // }
+            var ketQua = lopQuanLiDbContext.LopQuanLis
+                .Find(malop);
+            return ketQua == null ? NotFound() : Ok(ketQua);
         }
 
-        [HttpGet("info")]
-        public ActionResult GetBasicInfo()
+        [HttpGet("siso")]
+        public ActionResult GetAllWithSiSo()
         {
             // Lấy khoa, giảng viên
             var joinedResult = lopQuanLiDbContext.LopQuanLis
@@ -71,18 +53,90 @@ namespace qlsinhvien.Controllers
                                 join s in lopQuanLiDbContext.SinhViens
                                     on l.MaLopQuanLi equals s.MaLopQuanLi
                                 group l by l.MaLopQuanLi into grouped
-                                let lql = grouped.First()
-                                select new LopQuanLi()
+                                let lql = grouped.FirstOrDefault()
+                                select new 
                                 {
-                                    MaLopQuanLi = lql.MaLopQuanLi,
-                                    TenLopQuanLi = lql.TenLopQuanLi,
-                                    GiangVien = lql.GiangVien,
-                                    Khoa = lql.Khoa,
-                                    MaKhoa = lql.Khoa.MaKhoa,
-                                    MaGiangVien = lql.GiangVien.MaGiangVien,
+                                    lql.MaLopQuanLi,
+                                    lql.TenLopQuanLi,
+                                    lql.GiangVien,
+                                    lql.Khoa,
                                     SiSo = grouped.Count()
                                 };
             return groupByResult == null ? NotFound() : Ok(groupByResult);
+        }
+
+        [HttpPost]
+        public ActionResult AddLopQL([FromBody] LopQuanLi lopQuanLi)
+        {
+            // Bắt buộc khi thêm phải có : tên lớp, mã khoa
+            if (lopQuanLi.MaLopQuanLi != 0 || lopQuanLi.MaKhoa == 0)
+            {
+                return BadRequest();
+            }
+            var khoa = lopQuanLiDbContext.Khoas.Find(lopQuanLi.MaKhoa);
+            if (khoa == null)
+            {
+                return NotFound($"{nameof(Khoa)} {lopQuanLi.MaKhoa}");
+            }
+            if (lopQuanLi.MaGiangVien != 0)
+            {
+                var giangVien = lopQuanLiDbContext.GiangViens.Find(lopQuanLi.MaGiangVien);
+                if (giangVien == null)
+                {
+                    return NotFound($"{nameof(GiangVien)} {lopQuanLi.MaGiangVien}");
+                }
+                // giảng viên khoa này phải chủ nhiệm lớp thuộc khoa đó
+                if (giangVien.MaKhoa != lopQuanLi.MaKhoa)
+                {
+                    return BadRequest("Not match");
+                }
+            }
+            lopQuanLiDbContext.LopQuanLis.Add(lopQuanLi);
+            lopQuanLiDbContext.SaveChanges();
+            return Created(nameof(GetById), lopQuanLi);
+        }
+
+        [HttpPut]
+        public ActionResult UpdateLopQL([FromBody] LopQuanLi lopQuanLi) {
+            if (lopQuanLi.MaLopQuanLi != 0) {
+                return BadRequest();
+            }
+            var inDb = lopQuanLiDbContext.LopQuanLis.Find(lopQuanLi.MaLopQuanLi);
+            if (inDb == null) {
+                return NotFound();                
+            }
+            inDb.TenLopQuanLi = lopQuanLi.TenLopQuanLi;
+            inDb.MaKhoa = lopQuanLi.MaKhoa;
+            inDb.MaGiangVien = lopQuanLi.MaGiangVien;
+            lopQuanLiDbContext.SaveChanges();
+            return Ok(inDb);
+        }
+
+        [HttpDelete("{malop}")]
+        public ActionResult RemoveLopQL(int malop) {
+            var inDb = lopQuanLiDbContext.LopQuanLis.Find(malop);
+            if (inDb == null) {
+                return NotFound();
+            }   
+            lopQuanLiDbContext.LopQuanLis.Remove(inDb);
+            lopQuanLiDbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult RemoveLopQLByRange([FromBody] List<int> malops)
+        {
+            var lopQuanLis = new List<LopQuanLi>();
+            foreach (var malop in malops)
+            {
+                var inDb = lopQuanLiDbContext.LopQuanLis.Find(malop);
+                if (inDb != null) {
+                    lopQuanLis.Add(inDb);
+                }
+            }
+            lopQuanLiDbContext.LopQuanLis.RemoveRange(lopQuanLis);
+            lopQuanLiDbContext.SaveChanges();
+            return Ok();
         }
     }
 }
