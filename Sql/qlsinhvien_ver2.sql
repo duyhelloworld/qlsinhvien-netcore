073921df845a40212c84780364277810b8562fc1
Kaizu69 <hiep8ams@gmail.com>
2023-08-15 09:57:34 +0700

    ...

--- Sql/qlsinhvien_ver2.sql
+++ Sql/qlsinhvien_ver2.sql
@@ -1,4 +1,5 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+﻿-- Active: 1692067322401@@127.0.0.1@1433@qlsinhvien
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
 BEGIN
     CREATE TABLE [__EFMigrationsHistory] (
         [MigrationId] nvarchar(150) NOT NULL,
