722bfae74c8d034c13ade902b7bc41ca985e8021
duyhelloworld <codedaovoiduy@gmail.com>
2023-08-16 10:57:52 +0700

    viết lại, còn thiếu xoá

--- Controllers/GiangVienController.cs
+++ Controllers/GiangVienController.cs
@@ -3,133 +3,257 @@ using System.Runtime.Intrinsics.X86;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using qlsinhvien.Context;
+using qlsinhvien.Dto;
 using qlsinhvien.Entities;
-namespace qlsinhvien.Controllers
+namespace qlsinhvien.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+public class GiangVienController : ControllerBase
 {
-    [ApiController]
-    [Route("/[controller]")]
-    public class GiangVienController : ControllerBase
+    private readonly ApplicationContext giangVienDbContext;
+    public GiangVienController(ApplicationContext giangVienDbContext)
     {
-        private readonly ApplicationContext appContext;
-        public GiangVienController(ApplicationContext appContext)
-        {
-            this.appContext = appContext;
-        }
+        this.giangVienDbContext = giangVienDbContext;
+    }
 
-        [HttpGet]
-        public ActionResult<ICollection<GiangVien>> GetAll()
+    [HttpGet]
+    public ActionResult<ICollection<GiangVien>> GetAll()
+    {
+        return giangVienDbContext.GiangViens.ToList();
+    }
+
+    [HttpGet("{magiangvien}")]
+    public ActionResult GetById(int magiangvien)
+    {
+        var gv = giangVienDbContext.GiangViens.Find(magiangvien);
+        return gv == null ? NotFound() : Ok(gv);
+    }
+
+    [HttpGet("hoten")]
+    public ActionResult GetByName([FromQuery] string hoTen)
+    {
+        var gv = giangVienDbContext.GiangViens
+            .Where(GiangVien => GiangVien.HoTen.Contains(hoTen))
+            .OrderBy(g => g.MaGiangVien)
+            .ToList();
+        return gv.Count == 0 ? NotFound() : Ok(gv);
+    }
+
+    [HttpGet("bomon/{maBoMon}")]
+    public ActionResult GetByBoMon(int maBoMon)
+    {
+        var boMon = giangVienDbContext.BoMons.Find(maBoMon);
+        if (boMon == null) {
+            return NotFound($"Bộ môn mã số {maBoMon} không tồn tại");
+        }
+        giangVienDbContext.BoMons.Entry(boMon).Collection(bm => bm.GiangViens).Load();
+        var giangViens = boMon.GiangViens;
+        if (giangViens == null || giangViens.Count == 0)
         {
-            return appContext.GiangViens
-                // .Include(gv => gv.Khoa)
+            return NotFound();
+        }
+        var ketQua = from gv in giangViens
+                    select new {
+                        gv.MaGiangVien,
+                        gv.HoTen,
+                        gv.Email,
+                        gv.SoDienThoai,
+                        BoMon = new {
+                            gv.BoMon.MaBoMon,
+                            gv.BoMon.TenBoMon
+                        }
+                    };
+        return Ok(ketQua);
+    }
+
+    [HttpGet("lopquanli/{maLopQuanLi}")]
+    public ActionResult GetByLopQuanLi(int maLopQuanLi)
+    {
+        // var kq = from gv in giangVienDbContext.GiangViens
+        //          join lql in giangVienDbContext.LopQuanLis on gv.MaGiangVien equals lql.MaGiangVien
+        //          where lql.TenLopQuanLi.Contains(lopql)
+        //          select new
+        //          {
+        //              gv.MaGiangVien,
+        //              gv.HoTen,
+        //              gv.GioiTinh,
+        //              gv.Email,
+        //              lql.TenLopQuanLi
+        //          };
+        // return kq == null ? NotFound() : Ok(kq);
+        return NoContent();
+    }
+
+    [HttpGet("lopmh={lopmh}")]
+    public ActionResult GetByLopMonHoc(string lopmh)
+    {
+        // var kq = from gv in giangVienDbContext.GiangViens
+        //          join bm in giangVienDbContext.BoMons on gv.MaBoMon equals bm.MaBoMon
+        //          join mh in giangVienDbContext.MonHocs on bm.MaBoMon equals mh.MaBoMon
+        //          join lmh in giangVienDbContext.LopMonHocs on mh.MaMonHoc equals lmh.MaMonHoc
+        //          where lmh.TenLopMonHoc.Contains(lopmh)
+        //          select new
+        //          {
+        //              gv.MaGiangVien,
+        //              gv.HoTen,
+        //              gv.GioiTinh,
+        //              gv.Email,
+        //              lmh.TenLopMonHoc
+        //          };
+        // return kq == null ? NotFound() : Ok(kq);
+        return NoContent();
+    }
+
+    [HttpPost]
+    public ActionResult AddGiangVien([FromBody] GiangVienDto giangVienDto)
+    {
+        if (giangVienDto.MaGiangVien != 0 || giangVienDto.MaBoMon == 0)
+        {
+            return BadRequest("Chứa tham số không hợp lệ");
+        }
+        int maBoMon = giangVienDto.MaBoMon;
+        var maLopQuanLi = giangVienDto.MaLopQuanLi;
+        var boMon = giangVienDbContext.BoMons.Find(maBoMon);
+        if (boMon == null)
+        {
+            return NotFound($"Không tồn tại bộ môn có mã {maBoMon}!");
+        }
+        // Check tên, tuổi bla bla
+        var giangVien = new GiangVien() {
+            MaGiangVien = 0,
+            HoTen = giangVienDto.HoTen,
+            GioiTinh = giangVienDto.GioiTinh,
+            NgaySinh = giangVienDto.NgaySinh,
+            QueQuan = giangVienDto.QueQuan,
+            DiaChiThuongTru = giangVienDto.DiaChiThuongTru,
+            SoDienThoai = giangVienDto.SoDienThoai,
+            Email = giangVienDto.Email,
+            BoMon = boMon
+        };
+        // Thêm lớp quản lí
+        if (maLopQuanLi != 0)
+        {
+            var lopQuanLi = giangVienDbContext.LopQuanLis.Find(maLopQuanLi);
+            if (lopQuanLi == null)
+            {
+                return BadRequest($"Không tồn tại lớp quản lí có mã {maLopQuanLi}");
+            }
+            giangVien.LopQuanLi = lopQuanLi;
+        }
+        // Thêm các lớp môn học
+        var maLopMonHocs = giangVienDto.MaLopMonHocs;
+        if (maLopMonHocs != null && maLopMonHocs.Count() != 0) {
+            var lopMonHocs = giangVienDbContext.LopMonHocs
+                .Where(lmh => maLopMonHocs.Contains(lmh.MaLopMonHoc))
                 .ToList();
+            if (lopMonHocs.Count() != 0)
+            {
+                giangVien.LopMonHocs = lopMonHocs;
+            }
         }
-
-        [HttpGet("{magiangvien}")]
-        public ActionResult GetById(int magiangvien)
+        try
         {
-            var gv = appContext.GiangViens.Find(magiangvien);
-            return gv == null ? NotFound() : Ok(gv);
+            giangVienDbContext.GiangViens.Add(giangVien);
+            giangVienDbContext.SaveChanges();
         }
-
-        [HttpGet("ten={ten}")]
-        public ActionResult GetByName(string ten)
+        catch (DbUpdateException ex)
         {
-            Console.WriteLine(ten);
-            var gv = appContext.GiangViens.Where(GiangVien => GiangVien.HoTen.Contains(ten)).ToList();
-            return gv.Count == 0 ? NotFound() : Ok(gv);
+            if (ex.InnerException.Message.Contains("email")) {
+                return BadRequest($"Email {giangVienDto.Email} đã được sử dụng");
+            } else if (ex.InnerException.Message.Contains("SoDienThoai")) {
+                return BadRequest($"Số điện thoại {giangVienDto.SoDienThoai} đã được sử dụng");
+            }
+            throw;
         }
 
-
-        [HttpGet("khoa={khoa}")]
-        public ActionResult GetByKhoa(string khoa)
+        return CreatedAtAction(nameof(GetById),
+            new { 
+                giangVien.MaGiangVien, 
+                giangVien.HoTen,
+                giangVien.Email,
+                giangVien.BoMon.TenBoMon,
+                TenLopQuanLi = giangVien.LopQuanLi == null ? null : giangVien.LopQuanLi.TenLopQuanLi
+            });
+        }
+    
+    [HttpPut("{magiangvien}")]
+    public ActionResult UpdateThongTinGiangVien(int magiangvien, [FromBody] GiangVienDto giangVienDto)
+    {
+        var boMon = giangVienDbContext.BoMons.Find(giangVienDto.MaBoMon);
+        if (boMon == null)
         {
-            // var kq = from gv in appContext.GiangViens
-            //          join k in appContext.Khoas on gv.MaKhoa equals k.MaKhoa
-            //          where k.TenKhoa.Contains(khoa)
-            //          select new
-            //          {
-            //              gv.MaGiangVien,
-            //              gv.HoTen,
-            //              gv.GioiTinh,
-            //              gv.Email,
-            //              k.TenKhoa
-            //          };
-            // return kq == null ? NotFound() : Ok(kq);
-            return NoContent();
+            return NotFound($"Không tồn tại bộ môn có mã {giangVienDto.MaBoMon}!");
         }
-        [HttpGet("lopql={lopql}")]
-        public ActionResult GetByLopQuanLi(string lopql)
+        var gv = giangVienDbContext.GiangViens.Find(magiangvien);
+        if(gv == null)
         {
-            // var kq = from gv in appContext.GiangViens
-            //          join lql in appContext.LopQuanLis on gv.MaGiangVien equals lql.MaGiangVien
-            //          where lql.TenLopQuanLi.Contains(lopql)
-            //          select new
-            //          {
-            //              gv.MaGiangVien,
-            //              gv.HoTen,
-            //              gv.GioiTinh,
-            //              gv.Email,
-            //              lql.TenLopQuanLi
-            //          };
-            // return kq == null ? NotFound() : Ok(kq);
-            return NoContent();
+            return NotFound();
         }
+        giangVienDto.MaGiangVien = magiangvien;
+        gv.HoTen = giangVienDto.HoTen;
+        gv.GioiTinh = giangVienDto.GioiTinh;
+        gv.NgaySinh = giangVienDto.NgaySinh;
+        gv.QueQuan = giangVienDto.QueQuan;
+        gv.DiaChiThuongTru = giangVienDto.DiaChiThuongTru;
+        gv.SoDienThoai = giangVienDto.SoDienThoai;
+        gv.Email = giangVienDto.Email;
+        gv.BoMon = boMon;
+        // Ko cập nhật lớp quản lí / lớp môn học
+        giangVienDbContext.SaveChanges();
+        return Ok(new {
+            gv.MaGiangVien,
+            gv.HoTen,
+            gv.Email,
+            gv.BoMon.TenBoMon
+        });
+    }
 
-        [HttpGet("lopmh={lopmh}")]
-        public ActionResult GetByLopMonHoc(string lopmh)
+    [HttpPut("lopquanli/{magiangvien}")]
+    public ActionResult UpdateLopQuanLi_GiangVien(int magiangvien, [FromBody] int maLopQuanLi) {
+        if (maLopQuanLi == 0) {
+            return BadRequest();
+        }
+        var giangVien = giangVienDbContext.GiangViens.Find(magiangvien);
+        if (giangVien == null)
         {
-            // var kq = from gv in appContext.GiangViens
-            //          join bm in appContext.BoMons on gv.MaBoMon equals bm.MaBoMon
-            //          join mh in appContext.MonHocs on bm.MaBoMon equals mh.MaBoMon
-            //          join lmh in appContext.LopMonHocs on mh.MaMonHoc equals lmh.MaMonHoc
-            //          where lmh.TenLopMonHoc.Contains(lopmh)
-            //          select new
-            //          {
-            //              gv.MaGiangVien,
-            //              gv.HoTen,
-            //              gv.GioiTinh,
-            //              gv.Email,
-            //              lmh.TenLopMonHoc
-            //          };
-            // return kq == null ? NotFound() : Ok(kq);
-            return NoContent();
+            return NotFound();
         }
-        [HttpPost]
-        public ActionResult AddGiangVien([FromBody] GiangVien giangVien)
+        var lopQuanLi = giangVienDbContext.LopQuanLis.Find(maLopQuanLi);
+        if (lopQuanLi == null)
         {
-            // if (giangVien.MaGiangVien != 0 || giangVien.MaKhoa == 0)
-            // {
-            //     return BadRequest("Chứa tham số không hợp lệ");
-            // }
-            // try
-            // {
-            //     var khoa = appContext.Khoas.Find(giangVien.MaKhoa);
-
-            //     if (khoa == null)
-            //     {
-            //         return NotFound();
-            //     }
-            //     appContext.GiangViens.Add(giangVien);
-            //     appContext.SaveChanges();
-
-            //     return CreatedAtAction(nameof(GetById), new { maGiangVien = giangVien.MaGiangVien }, giangVien);
-            // }
-            // catch (HttpRequestException)
-            // {
-            //     return BadRequest();
-            // }
-            return NoContent();
+            return BadRequest($"Không tồn tại lớp quản lí có mã {maLopQuanLi}");
         }
-        
-        // [HttpPut("{magiangvien}")]
-        // public ActionResult UpdateGiangVien(int magiangvien)
-        // {
-        //     var gv = appContext.GiangViens.Find(magiangvien);
-        //     if(gv == null)
-        //     {
-        //         return NotFound();
-        //     }
+        giangVien.LopQuanLi = lopQuanLi;
+        giangVienDbContext.SaveChanges();
+        return Ok(giangVien);
+    }
 
-        // }
+    [HttpPut("lopmonhoc/{magiangvien}")]
+    public ActionResult UpdateLopMonHocs_GiangVien(int magiangvien, [FromBody] ICollection<int> maLopMonHocs) {
+        if (maLopMonHocs.Count == 0)
+        {
+            return BadRequest();
+        }
+        var giangVien = giangVienDbContext.GiangViens.Find(magiangvien);
+        if (giangVien == null)
+        {
+            return NotFound($"Không tồn tại giảng viên mã số {magiangvien}");
+        }
+        var lopMonHocs = giangVienDbContext.LopMonHocs
+                .Where(lmh => maLopMonHocs.Contains(lmh.MaLopMonHoc))
+                .ToList();
+        if (lopMonHocs.Count() == 0)
+        {
+            return NotFound("Không tồn tại các lớp môn học này.");
+        }
+        giangVien.LopMonHocs = lopMonHocs;
+        giangVienDbContext.SaveChanges();
+        return Ok(giangVien);
+    }
+
+    [HttpDelete("{magiangvien}")]
+    public ActionResult DeleteGiangVien(int magiangvien) {
+        return NoContent();
     }
 }
