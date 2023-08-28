4902e8276a697ed316445ce55ee79c7825a81a34
duyhelloworld <codedaovoiduy@gmail.com>
2023-08-28 10:22:28 +0700

    cấu hình identity

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using System.Text;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using qlsinhvien.Context;
+using qlsinhvien.Entities;
 using qlsinhvien.Services;
 using qlsinhvien.Services.Impl;
 
@@ -8,25 +13,69 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddJsonOptions(x =>
                 x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-// Service
+
+// Services
 builder.Services.AddScoped<IGiangVienService, GiangVienService>();
 builder.Services.AddScoped<IKhoaService, KhoaService>();
 builder.Services.AddScoped<IDiemSinhVienService, DiemSinhVienService>();
 builder.Services.AddScoped<ISinhVienService, SinhVienService>();
 builder.Services.AddScoped<IMonHocService, MonHocService>();
 
-// builder.Services.AddHttpClient("httpClient", client => {
-//     client.BaseAddress = new Uri("http://localhost:5277");
-// });
 builder.Services.AddDbContext<ApplicationContext>(options =>
 {
-    // options.EnableSensitiveDataLogging(true);
-    options
-        // .EnableDetailedErrors()
-        .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStrings"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStrings"));
+    options.EnableThreadSafetyChecks();
 });
+
+builder.Services.AddIdentity<NguoiDung, VaiTro>(option =>
+{
+    option.User.RequireUniqueEmail = true;
+
+    option.Password.RequireDigit = false;
+    option.Password.RequireNonAlphanumeric = false;
+    option.Password.RequireLowercase = false;
+    option.Password.RequireUppercase = false;
+    option.Password.RequiredUniqueChars = 0;
+    option.Password.RequiredLength = 5;
+
+    option.SignIn.RequireConfirmedAccount = false;
+    option.SignIn.RequireConfirmedEmail = false;
+    option.SignIn.RequireConfirmedPhoneNumber = false;
+
+    option.Lockout.MaxFailedAccessAttempts = 3;
+    option.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(30);
+})
+.AddEntityFrameworkStores<ApplicationContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.RequireAuthenticatedSignIn = true;
+}).AddJwtBearer(option =>
+{
+    option.SaveToken = true;
+    option.RequireHttpsMetadata = false;
+    option.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ClockSkew = TimeSpan.Zero,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudiences = builder.Configuration.GetSection("JWT:Audiences").Get<IEnumerable<string>>(),
+        IssuerSigningKey = new SymmetricSecurityKey(
+              Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]!)),
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
