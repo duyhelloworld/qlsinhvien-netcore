cfc2e812bdfcb3d4e70c0a5e802b633b5bb948ee
duyhelloworld <codedaovoiduy@gmail.com>
2023-08-29 10:18:48 +0700

    ~.~

--- Entities/Security/ModelDangNhap.cs
+++
@@ -1,7 +0,0 @@
-namespace qlsinhvien.Entities.Security;
-
-public class ModelDangNhap 
-{
-    public string TenNguoiDung  { get; set; } = null!;
-    public string MatKhau { get; set; } = null!;
-}
