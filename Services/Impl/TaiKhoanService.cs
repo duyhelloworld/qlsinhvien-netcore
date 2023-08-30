173d2d54a38b8c85adcb63788747c8c1f18cb6ad
duyhelloworld <codedaovoiduy@gmail.com>
2023-08-30 11:01:35 +0700

    OK

--- Services/Impl/TaiKhoanService.cs
+++ Services/Impl/TaiKhoanService.cs
@@ -24,16 +24,7 @@ public class TaiKhoanService : ITaiKhoanService
             MatKhau = model.MatKhau,
             TenHienThi = model.TenHienThi
         };
-        var result = await _userManager.CreateAsync(nguoiDung, nguoiDung.MatKhau);
-        if (!result.Succeeded)
-        {
-            Console.WriteLine(result.Errors.First());
-            return "";
-        }
-        // Thuật toán config ở Program.cs ?
-        var tokenBytes = await _userManager.CreateSecurityTokenAsync(nguoiDung);
-        var token = Convert.ToBase64String(tokenBytes);
-        return token;
+        return "";
     }
 
     public async Task<ModelTraVe> DangNhap(ModelDangNhap model)
@@ -43,25 +34,25 @@ public class TaiKhoanService : ITaiKhoanService
             TenNguoiDung = model.TenNguoiDung,
             MatKhau = model.MatKhau
         };
-        var result = await _signInManager.PasswordSignInAsync(nguoiDung, nguoiDung.MatKhau, false, false);
-        if (!result.Succeeded)
-        {
-            return new ModelTraVe() 
-            {
-                ThanhCong = false,
-                Data = "Sai tài khoản hoặc mật khẩu"
-            };
-        }
-        var token = Convert.ToBase64String(await _userManager.CreateSecurityTokenAsync(nguoiDung)); 
-        return new ModelTraVe() 
-        {
-            ThanhCong = true,
-            Data = token
-        };
+        // var result = await _signInManager.PasswordSignInAsync(nguoiDung, nguoiDung.MatKhau, false, false);
+        // if (!result.Succeeded)
+        // {
+        //     return new ModelTraVe() 
+        //     {
+        //         ThanhCong = false,
+        //         Data = "Sai tài khoản hoặc mật khẩu"
+        //     };
+        // }
+        // var token = Convert.ToBase64String(await _userManager.CreateSecurityTokenAsync(nguoiDung)); 
+        // return new ModelTraVe() 
+        // {
+        //     ThanhCong = true,
+        //     Data = token
+        // };
+        return null;
     }
 
     public async Task DangXuat(string token)
     {
-        await _signInManager.SignOutAsync();
     }
 }
