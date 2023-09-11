8fb8743967b2e3aacb26e7be28b6fd070c517834
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-11 16:37:56 +0700

    Refactor: triển khai 1 vài hàm service này

--- Services/Impl/LopMonHocService.cs
+++ Services/Impl/LopMonHocService.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using qlsinhvien.Context;
 using qlsinhvien.Dto;
+using qlsinhvien.Entities;
 using qlsinhvien.Exceptions;
 
 namespace qlsinhvien.Services.Impl;
@@ -16,37 +18,52 @@ public class LopMonHocService : ILopMonHocService
         _service = service;
     }
 
-    public Task<ActionResult> AddNewAsync(LopMonHocDto lopMonHocDto)
+    public async Task<LopMonHoc> AddNewAsync(LopMonHocDto lopMonHocDto)
+    {
+        var lopMonHoc = new LopMonHoc()
+        {
+            TenLopMonHoc = lopMonHocDto.TenLopMonHoc,
+        };
+        if (lopMonHocDto.MaGiangVien != 0)
+        {
+            var giangVien = await _context.GiangViens.FindAsync(lopMonHocDto.MaGiangVien)
+                ?? throw new ServiceException(404, $"Không có giảng viên mã số {lopMonHocDto.MaGiangVien}");
+            lopMonHoc.GiangVien = giangVien;
+        }
+        if (lopMonHocDto.MaMonHoc != 0)
+        {
+            var monHoc = await _context.MonHocs.FindAsync(lopMonHocDto.MaMonHoc)
+                ?? throw new ServiceException(404, $"Không có môn học mã số {lopMonHocDto.MaMonHoc}");
+            lopMonHoc.MonHoc = monHoc;
+        }
+        await _context.LopMonHocs.AddAsync(lopMonHoc);
+        await _context.SaveChangesAsync();
+        return lopMonHoc;
+    }
+
+    public async Task<IEnumerable<LopMonHoc>> GetAllAsync()
+    {
+        return await _context.LopMonHocs.ToListAsync();
+    }
+
+    public async Task<LopMonHoc?> GetByIdAsync(int maLopMonHoc)
+    {
+        return await _context.LopMonHocs.FindAsync(maLopMonHoc);
+    }
+
+    public Task<IEnumerable<LopMonHoc>> GetByTenAsync(string tenLopMonHoc)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ActionResult> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<ActionResult> GetByIdAsync(int maLopMonHoc)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<ActionResult> GetByTenAsync(string tenLopMonHoc)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<ActionResult> GetWithSiSoAsync()
+    public Task<IEnumerable<LopMonHoc>> GetWithSiSoAsync()
     {
         throw new NotImplementedException();
     }
     public async Task RemoveTheoMonHoc(int maMonHoc)
     {
-        var lop = await _context.LopMonHocs.FindAsync(maMonHoc);
-        if (lop == null)
-        {
-            throw new ServiceException(404, $"Không tồn tại lớp môn học đang dạy môn học có mã {maMonHoc}");
-        }
+        var lop = await _context.LopMonHocs.FindAsync(maMonHoc) 
+            ?? throw new ServiceException(404, $"Không tồn tại lớp môn học đang dạy môn học có mã {maMonHoc}");
         var Diems = from l in _context.LopMonHocs
                     join diem in _context.DiemSinhViens on l.MaLopMonHoc equals diem.MaLopMonHoc
                     where l.MaMonHoc == maMonHoc
@@ -77,12 +94,7 @@ public class LopMonHocService : ILopMonHocService
         await _context.SaveChangesAsync();
     }
 
-    public Task<ActionResult> RemoveRangeAsync(ICollection<int> maLopMonHocs)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<ActionResult> UpdateAsync(int maLopMonHoc, LopMonHocDto lopMonHocDto)
+    public Task<LopMonHoc> UpdateAsync(int maLopMonHoc, LopMonHocDto lopMonHocDto)
     {
         throw new NotImplementedException();
     }
