9a5e6f25e90cb3c97819477af768e455b81eca2c
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-11 22:28:07 +0700

    Refactor: Dùng range và required

--- Dto/DiemSinhVienDto.cs
+++ Dto/DiemSinhVienDto.cs
@@ -1,15 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace qlsinhvien.Dto;
 public class DiemSinhVienDto
 {
-    [JsonRequired]
+    [Range(1, 200)]
     public int MaLopMonHoc { get; set; }
-    [JsonRequired]
+    [Required]
+    [Range(1, 5000)]
     public int MaSinhVien { get; set; }
+    [Range(0, 30)]
     public float DiemChuyenCan { get; set; }
+    [Range(0, 10)]
     public float DiemGiuaKi { get; set; }
+    [Range(0, 10)]
     public float DiemCuoiKi { get; set; }
+    [Range(1, 8)]
     public int HocKi { get; set; }
     public string? GhiChu { get; set; }
 }
