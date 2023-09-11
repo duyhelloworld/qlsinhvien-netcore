ec575c8d239fd86146ae1138ec45e4e67e238fef
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-11 22:45:53 +0700

    Style: viết cho rõ nghĩa

--- Entities/TokenHetHan.cs
+++ Entities/TokenHetHan.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace qlsinhvien.Entities;
 
+[Table("TokenHetHan")]
 public class TokenHetHan
 {
     [Key]
     public string MaToken { get; set; } = null!;
-    public DateTime NgayHetHan { get; set; }  
+    public DateTime HetHanKhi { get; set; } = DateTime.Now;
 }
