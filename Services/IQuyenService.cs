83e9e0b7fb1179b388b5e5686052eb327ea73165
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-11 09:52:08 +0700

    Feat: thêm api cập nhật quyền

--- Services/IQuyenService.cs
+++ Services/IQuyenService.cs
@@ -7,6 +7,7 @@ public interface IQuyenService
     public Task<IEnumerable<Quyen>> LayTatCa();
     public Task<Quyen?> LayTheoTen(string TenQuyen);
     public Task ThemQuyen(QuyenDto quyenDto);
+    public Task CapNhatQuyen(string TenQuyen, QuyenDto quyenDto);
     public Task XoaQuyen(string TenQuyen);
 
 }
