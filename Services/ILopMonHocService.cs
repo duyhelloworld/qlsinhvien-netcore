049632381b6f9236e3529fabf2baa3103d2f18dd
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-11 16:39:17 +0700

    Fix: đặt kiểu dữ liệu trả về đúng với 1 service nên đặt

--- Services/ILopMonHocService.cs
+++ Services/ILopMonHocService.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using qlsinhvien.Dto;
+using qlsinhvien.Entities;
 
 namespace qlsinhvien.Services;
 
 public interface ILopMonHocService
 {
-    public Task<ActionResult> GetAllAsync();
-    public Task<ActionResult> GetWithSiSoAsync();
-    public Task<ActionResult> GetByIdAsync(int maLopMonHoc);
-    public Task<ActionResult> GetByTenAsync(string tenLopMonHoc);
-    public Task<ActionResult> AddNewAsync(LopMonHocDto lopMonHocDto);
-    public Task<ActionResult> UpdateAsync(int maLopMonHoc, LopMonHocDto lopMonHocDto);
+    public Task<IEnumerable<LopMonHoc>> GetAllAsync();
+    public Task<IEnumerable<LopMonHoc>> GetWithSiSoAsync();
+    public Task<LopMonHoc?> GetByIdAsync(int maLopMonHoc);
+    public Task<IEnumerable<LopMonHoc>> GetByTenAsync(string tenLopMonHoc);
+    public Task<LopMonHoc> AddNewAsync(LopMonHocDto lopMonHocDto);
+    public Task<LopMonHoc> UpdateAsync(int maLopMonHoc, LopMonHocDto lopMonHocDto);
     public Task RemoveAsync(int maLopMonHoc);
-    public Task<ActionResult> RemoveRangeAsync(ICollection<int> maLopMonHocs);
     public Task RemoveTheoMonHoc(int maMonHoc);
 }
