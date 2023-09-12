690e004ee95a2554b583b2a2bb09b8914c25b2f1
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-12 22:44:32 +0700

    Refactor: đặt ràng buộc allowNull cho 1 vài trường

--- Dto/MonHocDto.cs
+++ Dto/MonHocDto.cs
@@ -1,20 +1,16 @@
-using System.Text.Json.Serialization;
-using qlsinhvien.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace qlsinhvien.Dto;
 public class MonHocDto
 {
-    [JsonRequired]
+    [Required]
+    [Range(1, 200)]
     public int MaMonHoc { get; set; }
-    public string TenMonHoc { get; set; }
+    public string TenMonHoc { get; set; } = null!;
     public short SoTinChi { get; set; }
     public bool BatBuoc { get; set; }
-
-    public string MoTa { get; set; }
-
-    public int MaMonTienQuyet { get; set; }
-    [JsonRequired]
+    public string? MoTa { get; set; }
+    public int? MaMonTienQuyet { get; set; }
     public int MaBoMon { get; set; }
-
     public int MaLopMonHoc { get; set; }
 }
