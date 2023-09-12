75ba991b34185a622ccf3ca01901801e3ea5404e
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-12 22:43:37 +0700

    Feat: Người dùng service chạy thành công tất cả api

---
+++ Services/INguoiDungService.cs
@@ -0,0 +1,21 @@
+using qlsinhvien.Dtos;
+using qlsinhvien.Entities;
+
+namespace qlsinhvien.Services;
+
+public interface INguoiDungService
+{
+    public Task<IEnumerable<NguoiDungDto>> GetAll();
+    public Task<IEnumerable<NguoiDungDto>> GetAllNguoiDungChuaPhanQuyen();
+    public Task<IEnumerable<NguoiDungDto>> GetAllNguoiDungDaPhanQuyen();
+    public Task<IEnumerable<NguoiDungDetail>> GetAllGroupByVaiTro();
+    public Task<IEnumerable<NguoiDungDto>> GetByTenNguoiDung(string TenNguoiDung);
+    public Task<IEnumerable<NguoiDungDto>> GetByTenHienThi(string TenHienThi);
+    public Task<IEnumerable<NguoiDungDto>> GetByVaiTro(string TenVaiTro);
+    public Task Them(NguoiDungDtoDangKi nguoiDungDtoDki);
+    public Task PhanQuyen(ModelCapQuyen modelCapQuyen);
+    public Task ThemVaPhanQuyen(NguoiDungDtoDangKi nguoiDungDtoDki);
+    public Task HuyPhanQuyen(string TenNguoiDung);
+    public Task CapNhatThongTin(string TenNguoiDung, NguoiDungDto nguoiDungDto);
+    public Task Xoa(string TenNguoiDung);
+}
