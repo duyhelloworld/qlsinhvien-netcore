6411ec9edadee845ebf52e47aba729ded6941ba5
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-15 11:14:09 +0700

    Feat: thêm tên vai trò, hàm convert trong dto

--- Dto/QuyenDto.cs
+++ Dto/QuyenDto.cs
@@ -8,4 +8,25 @@ public class QuyenDto
     public string TenQuyen { get; set; } = null!;
 
     public string? GhiChu { get; set; }
+
+    public IEnumerable<string>? TenVaiTros { get; set; } = null!;
+
+    public static QuyenDto Convert(Quyen quyen)
+    {
+        return new QuyenDto()
+        {
+            TenQuyen = quyen.TenQuyen,
+            GhiChu = quyen.GhiChu,
+            TenVaiTros = quyen.VaiTros.Select(vt => vt.TenVaiTro)
+        };
+    }
+
+    public static Quyen Convert(QuyenDto quyenDto)
+    {
+        return new Quyen()
+        {
+            TenQuyen = quyenDto.TenQuyen,
+            GhiChu = quyenDto.GhiChu
+        };
+    }
 }
