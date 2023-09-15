58fa084e15d02784d6ad8684ba43f56adba5b227
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-15 11:11:41 +0700

    Refactor: đơn giản lại logic khi call giangvien

--- Controllers/GiangVienController.cs
+++ Controllers/GiangVienController.cs
@@ -44,26 +44,15 @@ public class GiangVienController : ControllerBase
         var giangVien = await _service.GetById(phanQuyen.MaNguoiDung);
         if (giangVien == null)
         {
-            return BadRequest();
+            return NotFound();
         }
         return Ok(giangVien);
-    }
-
-    [HttpGet("hoten/{hoten:length(1, 40)}")]
-    [PhanQuyen(EQuyen.XemTheoTen_GiangVien)]
-    public async Task<IEnumerable<GiangVien>> GetByNameViaRoute(string hoTen)
-    {
-        return await _service.GetByTen(hoTen);
     }
     
     [HttpGet("hoten")]
     [PhanQuyen(EQuyen.XemTheoTen_GiangVien)]
-    public async Task<IEnumerable<GiangVien>> GetByNameViaQuery([FromQuery] string hoTen)
+    public async Task<IEnumerable<GiangVien>> GetByTen([FromQuery] string hoTen)
     {
-        if (hoTen.Length < 1 || hoTen.Length > 40)
-        {
-            return Enumerable.Empty<GiangVien>().ToList();
-        }
         return await _service.GetByTen(hoTen);
     }
 
@@ -71,76 +60,63 @@ public class GiangVienController : ControllerBase
     [PhanQuyen(EQuyen.XemTheoBoMon_GiangVien)]
     public async Task<IActionResult> GetByBoMonAsync(int maBoMon)
     {
-        var giangViens = await _service.GetByBoMon(maBoMon);
-        return Ok(giangViens);
+        return Ok(await _service.GetByBoMon(maBoMon));
     }
 
     [HttpGet("lopquanli/{maLopQuanLi:int:min(1)}")]
     [PhanQuyen(EQuyen.XemTheoLopQuanLi_GiangVien)]
     public async Task<IActionResult> GetByLopQuanLiAsync(int maLopQuanLi)
     {
-        var giangViens = await _service.GetByLopQuanLi(maLopQuanLi);
-        return Ok(giangViens);
+        return Ok(await _service.GetByLopQuanLi(maLopQuanLi));
     }
 
     [HttpGet("{magiangvien:int:min(1)}/lopmonhoc")]
     [PhanQuyen(EQuyen.XemLopMonHoc_GiangVien)]
     public async Task<IActionResult> GetLopMonHocs(int maGiangVien)
     {
-        var ketQua = await _service.GetLopMonHoc(maGiangVien);
-        return Ok(ketQua);        
+        return Ok(await _service.GetLopMonHoc(maGiangVien));        
     }
     
     [HttpGet("lopmonhoc/{malopmonhoc:int:min(1)}")]
     [PhanQuyen(EQuyen.XemTheoLopMonHoc_GiangVien)]
     public async Task<IActionResult> GetByLopMonHoc(int maLopMonHoc)
     {
-        var ketQua = await _service.GetByLopMonHoc(maLopMonHoc);
-        if (ketQua == null) 
-        {
-            return NotFound();
-        }
-        return Ok(ketQua);
+        return Ok(await _service.GetByLopMonHoc(maLopMonHoc));
     }
 
     [HttpPost]
     [PhanQuyen(EQuyen.ThemMoi_GiangVien)]
     public async Task<IActionResult> AddGiangVien([FromBody] GiangVienDto giangVienDto)
     {
-        var ketQua = await _service.AddNew(giangVienDto);
-        return Ok(ketQua);
+        return Ok(await _service.AddNew(giangVienDto));
     }
 
     [HttpPost("{magiangvien:int:min(1)}/lopmonhoc")]
     [PhanQuyen(EQuyen.ThemMoiLopMonHoc_GiangVien)]
     public async Task<IActionResult> ThemLopMonHocs_GiangVien(int magiangvien, [FromBody] ICollection<int> maLopMonHocs)
     {
-        var ketQua = await _service.AddNewLopMonHoc(magiangvien, maLopMonHocs);
-        return Ok(ketQua);
+        return Ok(await _service.AddNewLopMonHoc(magiangvien, maLopMonHocs));
     }
 
     [HttpPut("{magiangvien:int:min(1)}")]
     [PhanQuyen(EQuyen.SuaProfile_GiangVien)]
     public async Task<IActionResult> UpdateThongTinGiangVien(int magiangvien, [FromBody] GiangVienDto giangVienDto)
     {
-        var ketQua = await _service.UpdateProfile(magiangvien, giangVienDto);
-        return Ok(ketQua);
+        return Ok(await _service.UpdateProfile(magiangvien, giangVienDto));
     }
 
     [HttpPut("{magiangvien:int:min(1)}/lopquanli/{malopquanli:int:min(1)}")]
     [PhanQuyen(EQuyen.SuaLopQuanLi_GiangVien)]
     public async Task<IActionResult> UpdateLopQuanLi_GiangVien( int magiangvien,  int maLopQuanLi)
     {
-        var ketQua = await _service.UpdateLopQuanLi_GiangVien(magiangvien, maLopQuanLi);
-        return Ok(ketQua);
+        return Ok(await _service.UpdateLopQuanLi_GiangVien(magiangvien, maLopQuanLi));
     }
 
     [HttpPut("{magiangvien:int:min(1)}/bomon/{mabomon:int:min(1)}")]
     [PhanQuyen(EQuyen.SuaBoMon_GiangVien)]
     public async Task<IActionResult> UpdateBoMon_GiangVien( int magiangvien,  int maBoMon)
     {
-        var ketQua = await _service.UpdateBoMon_GiangVien(magiangvien, maBoMon);
-        return Ok(ketQua);
+        return Ok(await _service.UpdateBoMon_GiangVien(magiangvien, maBoMon));
     }
 
     [HttpDelete("{magiangvien:int:min(1)}")]
