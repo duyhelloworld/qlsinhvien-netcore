db6646342ae3b028ded197cd6dabff5d5ba0f2f1
duyhelloworld <codedaovoiduy@gmail.com>
2023-09-21 09:46:50 +0700

    Feat: thêm 3 cái get cụ thể hơn

--- Services/IBoMonService.cs
+++ Services/IBoMonService.cs
@@ -8,6 +8,9 @@ public interface IBoMonService
 {
     public Task<IEnumerable<BoMon>> GetAllAsync();
     public Task<BoMon?> GetTheoMa(int MaBoMon);
+    public Task<BoMon?> GetTheoMonHoc(int MaMonHoc);
+    public Task<BoMon?> GetTheoGiangVien(int MaGiangVien);
+    public Task<IEnumerable<BoMon>> GetTheoKhoa(int MaKhoa);
     public Task<BoMon> Add(BoMonDto boMon);
     public Task<BoMon> UpdateTen(int MaBoMon, string TenBoMon);
     public Task<BoMon> UpdateGiangVien(int MaBoMon, IEnumerable<int> MaGiangViens);
